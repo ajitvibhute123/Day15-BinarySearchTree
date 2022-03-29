ab14df558851eae119754dc018283848590c9061
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-03-29 20:02:15 +0530

    Added : Insert Elements in BST

---
+++ BSTImplementation.java
@@ -0,0 +1,18 @@
+
+package com.bridgelabz;
+
+public class BSTImplementation {
+    /*
+     * @Purpose : Insert Elements in BST
+     */
+    public static void main(String[] args) {
+        BST tree = new BST();
+        Node root = null;
+        root = tree.insert(root, 56);
+        root = tree.insert(root, 30);
+        root = tree.insert(root, 70);
+
+        tree.printInorder(root);
+        System.out.println(root);
+    }
+}
